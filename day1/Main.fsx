497c9df2d85ab6759b3a448148950c7be2ea37cc
Sven Baumgartner <caos666inside@yahoo.it>
2019-12-04 13:38:12 +0100

    added first days

---
+++ day1/Main.fsx
@@ -0,0 +1,15 @@
+open System
+
+let calculateFuelForMass (mass: int) = mass / 3 - 2
+
+let rec calculateFuelWithFuel mass =
+    let fuel = calculateFuelForMass mass
+    if fuel <= 0 then 0
+    else fuel + calculateFuelWithFuel fuel
+
+let readFileInput filePath = System.IO.File.ReadLines(filePath)
+
+let calculateCounterUpper filePath =
+    readFileInput filePath
+    |> Seq.map Convert.ToInt32
+    |> Seq.sumBy calculateFuelWithFuel
